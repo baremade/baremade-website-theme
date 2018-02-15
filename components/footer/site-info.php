250c3c2179fb79a319196f98ed7285d02bea6067
Jon-Pierre Sanchez <jp@animoto.com>
2018-02-14 22:20:47 -0500

    Adding more util classes for landing page use case + updated to footer copyright

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,3 +1,3 @@
 <div class="col-12 center">
-	<p class="tiny copyright">Copyright Michelle Sanchez, LLC   |   <a href="/privacy-policy-legal-disclaimer">Privacy Policy &amp; Legal</a></p>
+	<p class="tiny copyright">Copyright Baremade, LLC   |   <a href="/privacy-policy-legal-disclaimer">Privacy Policy &amp; Legal</a></p>
 </div>

d0dc3e364c018be941d0290246e888caeccad7b7
Jon-Pierre Sanchez <jp@animoto.com>
2018-02-16 23:47:58 -0500

    Updating style sheet version

--- functions.php
+++ functions.php
@@ -205,7 +205,7 @@ add_action( 'widgets_init', 'base_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function base_theme_scripts() {
-	wp_enqueue_style( 'base_theme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'base_theme-style', get_stylesheet_uri(), array(), '20180216' );
 
 	wp_enqueue_script( 'base_theme-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array('jquery'), '20151215', true );
 
